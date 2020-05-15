1bb131a6433486cbfa14c85a125821b653905001
Hrushikesh Tiwari <hrushikesh.tiwari@gmail.com>
2020-05-15 11:33:25 +0530

    Updates

---
+++ precache-manifest.f027c771f023444080637724c40eeaa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02176ddeb2d99e0dcb92e2c10db6ea05",
+    "url": "/LiveCoronaTracker/index.html"
+  },
+  {
+    "revision": "db3028b9157498592e75",
+    "url": "/LiveCoronaTracker/static/css/2.5460d2f5.chunk.css"
+  },
+  {
+    "revision": "57a9d36ab82d4014a9d1",
+    "url": "/LiveCoronaTracker/static/css/main.c9adb9ee.chunk.css"
+  },
+  {
+    "revision": "db3028b9157498592e75",
+    "url": "/LiveCoronaTracker/static/js/2.e849e178.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/LiveCoronaTracker/static/js/2.e849e178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a9d36ab82d4014a9d1",
+    "url": "/LiveCoronaTracker/static/js/main.bfba48f9.chunk.js"
+  },
+  {
+    "revision": "9dde36daaa5e4d28b971",
+    "url": "/LiveCoronaTracker/static/js/runtime-main.28ec5c5c.js"
+  }
+]);
